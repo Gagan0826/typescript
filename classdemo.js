b04034b482c6df433744480cbf21b3808e0d0a54
Gagan <gaganbrgirish26@gmail.com>
2023-10-25 21:33:16 +0530

    class modified

---
+++ classdemo.js
@@ -0,0 +1,11 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var student = /** @class */ (function () {
+    function student(sid, name) {
+        this.studentId = sid;
+        this.name = name;
+    }
+    return student;
+}());
+var Gagan = new student("12si2112", "Gagan");
+console.log(Gagan);
---
+++ classdemo.ts
@@ -0,0 +1,14 @@
+class student
+{
+    studentId:string
+    name:string
+    constructor(sid,name)
+    {
+        this.studentId=sid
+        this.name=name
+    }
+}
+
+const Gagan=new student("12si2112","Gagan")
+console.log(Gagan)
+export {}
